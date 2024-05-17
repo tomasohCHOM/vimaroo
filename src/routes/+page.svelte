1680d2645509ae214e64f79a884a7cd2ba1a978f
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-05-17 14:49:15 -0700

    Add remaining colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,38 @@
 <script lang="ts">
   import Editor from "$lib/components/editor.svelte";
+
+  let modes = [
+    "words",
+    "containers",
+    "relative",
+    "hjkl",
+    "whackamole",
+    "mixed",
+  ];
+  let selectedOption = 2;
 </script>
 
-<h1>VIMaestro</h1>
+<main class="max-w-screen-lg flex flex-col pt-12">
+  <h1 class="font-bold text-3xl">VIMaestro</h1>
 
-<div class="editor">
-  <Editor />
-</div>
+  <div
+    class="flex items-center justify-center bg-secondary gap-2 rounded-lg px-4 py-2"
+  >
+    {#each modes as mode, i}
+      <button
+        class="font-semibold {i === selectedOption ? 'text-contrast_b' : ''}"
+        >{mode}</button
+      >
+    {/each}
+  </div>
+
+  <div class="editor">
+    <Editor />
+  </div>
+</main>
 
 <style>
   .editor {
-    max-width: 1000px;
     height: 400px;
   }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgb(var(--color-primary))",
+        secondary: "rgb(var(--color-secondary))",
+        contrast: "rgb(var(--color-contrast))",
+        contrast_g: "rgb(var(--color-contrast-g))",
+        contrast_b: "rgb(var(--color-contrast-b))",
+      },
+      fontFamily: {
+        general: "var(--ff-general)",
+        code: "var(--ff-code)",
+      },
+    },
   },
   plugins: [],
 };
