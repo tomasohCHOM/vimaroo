b5efebe67829ca82ff4caf767fa2d83ee014b0ec
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-07-30 13:47:05 -0700

    Add website logo

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -5,7 +5,10 @@
 </script>
 
 <nav class="flex items-center justify-between px-4 md:px-14">
-	<h1 class="text-3xl font-bold">vimaroo</h1>
+	<h1 class="flex gap-2 text-3xl font-bold">
+		<span class="z-10">vimaroo</span>
+		<img src="/favicon.svg" alt="Favicon" class="w-10" />
+	</h1>
 	<div class="flex items-center justify-center gap-1">
 		<button>
 			<Icon icon="mdi:about-circle-outline" width={24} />
