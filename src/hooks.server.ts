778b6607a34fc62502c0e55e58bc8a5641ff1f2c
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-08-30 10:16:23 -0700

    Suppress supabase session warning

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -26,6 +26,15 @@ const supabase: Handle = async ({ event, resolve }) => {
 		}
 	});
 
+	if ("suppressGetSessionWarning" in event.locals.supabase.auth) {
+		// @ts-expect-error - suppressGetSessionWarning is not part of the official API
+		event.locals.supabase.auth.suppressGetSessionWarning = true;
+	} else {
+		console.warn(
+			"SupabaseAuthClient#suppressGetSessionWarning was removed. See https://github.com/supabase/auth-js/issues/888."
+		);
+	}
+
 	/**
 	 * Unlike `supabase.auth.getSession()`, which returns the session _without_
 	 * validating the JWT, this function also calls `getUser()` to validate the
